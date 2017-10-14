151cd34f13c35bcabd3fdec9842ddb522ce9fb7a
jonas-git <jonas.vanen@gmail.com>
2017-10-14 21:07:30 +0200

    Added comment explaining purpose

--- manifest.bat
+++ manifest.bat
@@ -1,3 +1,7 @@
+rem This script is used to write a manifest file into an executable
+rem Usage: $ ./manifest <executable-path>
+rem <executable-path>.manifest is expected to exist
+
 @echo off
 "C:\Program Files (x86)\Windows Kits\8.1\bin\x64\mt.exe" ^
     -nologo -manifest "%1.manifest" -outputresource:"%1"
